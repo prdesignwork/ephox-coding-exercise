0f5a39300a4921acbaea5787e25a4e2f34ad18d5
Paul Rodriguez <paul@prdesign.work>
2018-04-19 11:54:02 -0600

    creating new tinymce dialog object

--- public/app.js
+++ public/app.js
@@ -2,4 +2,33 @@ tinymce.init({
   selector:'#editor',
   width: 600,
   height: 400,
-});
+  setup: function(editor) {
+    editor.addButton('addName', {
+      text: 'Insert User Name',
+      icon: false,
+      onclick: function() {
+        editor.windowManager.open({
+          title: 'Insert User Name',
+          body: [{
+              type: 'textbox',
+              name: 'firstName',
+              label: 'First Name'
+            },
+            {
+              type: 'textbox',
+              name: 'lastName',
+              label: 'Last Name'
+            }
+          ],
+          width: 700,
+          height: 100,
+          onsubmit: function(e) {
+            editor.insertContent(e.data.firstName)
+            editor.insertContent(' ')
+            editor.insertContent(e.data.lastName)
+          },
+        })
+      }
+    })
+  }
+})
